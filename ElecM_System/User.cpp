7c20a4512131d730f5eb7559b6eebdc548428dee
a110b1110 <q970806@daum.net>
2017-09-19 15:20:12 +0900

    개발 프로젝트 기본 요구사항

--- ElecM_System/User.cpp
+++ ElecM_System/User.cpp
@@ -5,9 +5,25 @@ char* User::GetName(void)
 	return userName;
 }
 
+void User::is_Init(void)
+{
+	if (head->next != NULL)
+	{
+		head->next = new User();
+	}
+	else {
+		cout << " error !" << endl;
+	}
+}
+
 User::User()
 {
 	userName = new char[20];
+	head = new User();
+	tail = new User();
+
+	head->next = tail;
+	tail->prev = head;
 }
 
 User::~User()
--- ElecM_System/User.h
+++ ElecM_System/User.h
@@ -1,17 +1,25 @@
 #pragma once
 #ifndef  __USER_H__
 #define __USER_H__
+#include <iostream>
 using namespace std;
 
 class User
 {
 private:
 	char*	userName;		// 유저이름
-
+public :
+	User*	prev;
+	User*	next;
+	
 public:
-	char* GetName(void);
+	char*	GetName(void);
+	void	is_Init(void);
 public:
 	User();
 	~User();
 };
+
+User* head;
+User* tail;
 #endif // ! __USER_H__
